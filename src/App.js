2209e41635588be6e7b7a1d6d4112a40c85d6eeb
Leandro <leandro_csilva@ymail.com>
2021-09-04 01:36:57 -0300

    Instalado Bootstrap e Ad Layout principal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <h1>Interface Bitbucket</h1>
+    <Container fluid>
+      <Row>
+        <Col className="perfil-menu" xs={12} md={3}>
+            <Row>
+              <Col xs="auto">
+                <input type="text" placeholder="Buscar usuário"/>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Perfil
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Menu
+              </Col>
+            </Row>          
+        </Col>
+        <Col className="conteudo" xs={12} md={9}>
+          Lista Repositório
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
