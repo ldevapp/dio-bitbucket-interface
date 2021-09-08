c90cdf1802d515123dd2ce8f3babb39528b28f98
Leandro <leandro_csilva@ymail.com>
2021-09-07 22:39:48 -0300

    Ad link do projeto e format data dos itens lista

--- src/Components/RepositoryList/ListItem.js
+++ src/Components/RepositoryList/ListItem.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import {Image} from 'react-bootstrap';
+var dateFormat = require("dateformat");
 
-export default function ListItem({avatar, name, description, updated}) {
+export default function ListItem({avatar, name, description, updated, href}) {
+    let dateUpdated = new Date(updated);
     return (
         <tr>
             <td>
                 <Image src={(avatar)? avatar : 'images/avatar-profile.jpg'} width="30px" rounded /> 
-                &nbsp;{name}
+                &nbsp;
+                <a href={href} target="_blank"  rel="noreferrer">{name}</a>
             </td>
             <td>{description}</td>
-            <td>{updated}</td>
+            <td>{dateFormat(dateUpdated, "mm/dd/yyyy HH:MM:ss")}</td>
         </tr>
     )
 }
